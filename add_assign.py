b8f6895ef29930198e9eb3d408647157e93be077
Chris Baca <cbaca@Chriss-MacBook-Pro.local>
2022-03-28 11:28:40 -0500

    built structure for adding assignments, need to fix json

--- add_assign.py
+++ add_assign.py
@@ -1,75 +1,121 @@
 import requests
 import json
 from requests.packages import urllib3
-import credentials
+import get_token, db_connect
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-# Base variables/urls
-
-key = credentials.key
-secret = credentials.secret
-payload = {
-            'grant_type':'client_credentials'
-        }
-base_url = 'blackboard.sagu.edu'
+cur = None
+conn = None
 
 # API Endpoints / string variables
 
+base_url = 'blackboard.sagu.edu'
 assign_path_one = '/learn/api/public/v1/courses/courseId:'
 course_id = 'grade_test_course'
 assign_path_two = '/contents/createAssignment'
-oauth_path = '/learn/api/public/v1/oauth2/token'
 
 # Authorize script and get access token
 
-session = requests.session()
-r = session.post('https://'+ base_url + oauth_path, data=payload, auth=(key, secret), verify=False)
-print("[auth:setToken()] STATUS CODE: " + str(r.status_code))
-res = json.loads(r.text)
-print("[auth:setToken()] RESPONSE: \n" + json.dumps(res,indent=4, separators=(',', ': ')))
-
-token = res['access_token']
-authStr = 'Bearer ' + token
-
 # Query for finding Formative Assessment (changing to Learning Assessment) folders... or whatever other folders
 # TODO: use db_connect.py to connect to database and find folders to add the assignment to
+def folder_list():
+    term_wildcard = input("What course or group of courses do you want to work with? ")
+    folder_label = input("What folder in this course or these courses do you want to put the assignment in? ")
 
-query = """select
-        cm.pk1
-        ,toc.label
-        ,toc.contentId
-        ,cm.course_id
-        ,t.name as term
-        from course_main cm
-            inner join course_term ct on ct.crsmain_pk1 = cm.pk1
-            inner join term t on t.pk1 = ct.term_pk1
-            left join (
-                        select
-                        toc.crsmain_pk1
-                        ,concat('_',toc.course_contents_pk1,'_1') as contentId
-                        ,toc.label
-                        from course_toc toc
-                        where toc.label like '%Formative Assessment%' 
-                        ) toc on toc.crsmain_pk1 = cm.pk1
-        where cm.course_id like '%dummy%'
-            and cm.pk1 not in (select crsmain_pk1 from course_course)
-        order by cm.course_id"""
+    query = """select
+            cm.pk1
+            ,toc.label
+            ,toc.contentId
+            ,cm.course_id
+            ,t.name as term
+            from course_main cm
+                inner join course_term ct on ct.crsmain_pk1 = cm.pk1
+                inner join term t on t.pk1 = ct.term_pk1
+                left join (
+                            select
+                            toc.crsmain_pk1
+                            ,concat('_',toc.course_contents_pk1,'_1') as contentId
+                            ,toc.label
+                            from course_toc toc
+                            where toc.label like '%{0}%' 
+                            ) toc on toc.crsmain_pk1 = cm.pk1
+            where cm.course_id like '%{1}%'
+                and cm.pk1 not in (select crsmain_pk1 from course_course)
+        order by cm.course_id""".format(folder_label, term_wildcard)
 
+    folder_list = []
 
-# open json file produced from get_data.py, use keys as course_id, use value as json string input for API
-# TODO: change the json here, or add json directly to script
-with open("data/bb_course_data.json", "r") as read_file:
-    bb_data = json.load(read_file)
+    cur = db_connect.connect()
 
-for k,v in bb_data.items():
-    course_id = k
-    course_specific_data = v
-    r = session.post('https://' + base_url + assign_path_one + course_id + assign_path_two,
-                 data=json.dumps(course_specific_data),
-                 headers={'Authorization':authStr, 'Content-Type':'application/json'},
-                 verify=False
-                )
-    print(f"Status Code: {r.status_code}, Response: {r.json()}")
+    cur.execute(query)
+    print("List of courses with folder that will have assignment added:")
+    for record in cur.fetchall():
+        print(record)
 
+    print("--------------------------")
+    answer = input("Do you want to add a new assignment to these courses? y/n ")
 
+    if answer == 'y':
+        cur.execute(query)
+        for record in cur.fetchall():
+            print("Appending to list: {0}".format(record))
+            folder_list.append(record)
+    elif answer == 'n':
+        pass
+    else:
+        print("Not a valid answer, no folders will be added.")
+
+    return (folder_list)
+
+def main():
+    # create data folder if not created and get auth token
+    get_token.data_folder()
+    auth_token = get_token.store_token()
+
+    # get folder/course list to add course
+    folders_and_courses = folder_list()
+
+    # add assignments to folders
+    session = requests.session()
+    for record in folders_and_courses:
+        # assignment json
+        j = """"{
+                "parentId": "{0}",
+                "title": "Learning Assessment 1",
+                "instructions": "<h4>Test Instructions for the Assignment</h4>",
+                "description": "test description",
+                "position": 0,
+                "availability":
+                    {"available": "Yes",
+                    "allowGuests": true,
+                    "adaptiveRelease":
+                        {"start": "2022-02-07T17:07:19.365Z",
+                        "end": "2022-02-07T17:07:19.365Z"
+                        }
+                    },
+                "grading":
+                    {"due": "2022-02-07T17:07:19.365Z",
+                    "attemptsAllowed": 0,
+                    "isUnlimitedAttemptsAllowed": true
+                    },
+                "score":
+                    {"possible": 5
+                    }
+                }""".format(record[2])
+        j = json.loads(j)
+
+        # add assignment via API
+        r = session.post('https://' + base_url + assign_path_one + course_id + assign_path_two,
+                     data=json.dumps(j),
+                     headers={'Authorization':auth_token, 'Content-Type':'application/json'},
+                     verify=False
+                    )
+        print(f"Status Code: {r.status_code}, Response: {r.json()}")
+
+if __name__ == '__main__':
+    main()
+
+if cur is not None:
+    cur.close()
+if conn is not None:
+    conn.close()
